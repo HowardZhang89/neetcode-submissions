96bea94af5b1e200c7d75708ea28a3d4e021e525
Howard Zhang <43189663+HowardZhang89@users.noreply.github.com>
2026-07-23 15:42:35 -0500

    Add: baseball-game - submission-2

---
+++ Data Structures & Algorithms/baseball-game/submission-2.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        int score = 0;
+        Stack<Integer> stack = new Stack<>();
+        for(String operation : operations){
+            if(operation.equals("+")){
+                int top = stack.pop();
+                int newTop = top + stack.peek();
+                stack.push(top);
+                stack.push(newTop);
+                score += newTop;
+            }else if(operation.equals("C")){
+                score -= stack.pop();
+            }else if(operation.equals("D")){
+                stack.push(stack.peek() * 2);
+                score += stack.peek();
+            }else{
+                stack.push(Integer.parseInt(operation));
+                score += stack.peek();
+            }
+        }
+        return score;
+    }
+}
