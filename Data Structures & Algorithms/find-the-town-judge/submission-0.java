33d84ae3c9426d604039a1f7ed4f07e8b6edaa37
Howard Zhang <43189663+HowardZhang89@users.noreply.github.com>
2026-08-19 17:03:27 -0500

    Add: find-the-town-judge - submission-0

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        // judge must be trusted n times
+        // judge must not trust anybody else
+
+        int[] incoming = new int[n+1];
+        int[] outgoing = new int[n+1];
+
+        for(int i = 0; i < trust.length; i++){
+            outgoing[trust[i][0]]++;
+            incoming[trust[i][1]]++;
+        }
+
+        for(int i = 1; i <= n; i++){
+            if(incoming[i] == n-1 && outgoing[i] == 0){
+                return i;
+            }
+        }
+        return -1;
+
+
+
+    }
+}
